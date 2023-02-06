1dc1f742af9074c7243ec860403f7c8879a0f479
Gabriel <gabo2023brazil@gmail.com>
2023-02-06 16:45:46 -0500

    Woriking

--- src/App/index.js
+++ src/App/index.js
@@ -9,93 +9,7 @@ import { TallerGrades } from '../List/Item/TallerGrades';
 import { Routes, Route, HashRouter } from 'react-router-dom';
 import { LoginPage } from '../LoginPage/index';
 import { Menu } from '../Menu';
-
-const courses = [
-    {
-        nombre: 'Curso 1',
-        profesor: 'Mauro',
-        programa: 'Maestria en I.A',
-        workGrades: [
-            {
-                value: '1.0',
-            },
-            {
-                value: '2.9'
-            }, 
-            {
-                value: '4.5'
-            },
-            {
-                value: '3.2'
-            }
-        ],
-        examGrades: [
-            {
-                value: '1.0',
-            },
-            {
-                value: '2.9'
-            }, 
-            {
-                value: '4.5'
-            }
-        ],
-    },
-    {
-        nombre: 'Curso 2',
-        profesor: 'Mauro',
-        programa: 'Maestria en I.A',
-        workGrades: [
-            {
-                value: '1.0',
-            },
-            {
-                value: '2.9'
-            }, 
-            {
-                value: '4.5'
-            }
-        ],
-        examGrades: [
-            {
-                value: '1.0',
-            },
-            {
-                value: '2.9'
-            }, 
-            {
-                value: '4.5'
-            }
-        ],
-    },
-    {
-        nombre: 'Curso 3',
-        profesor: 'Mauro',
-        programa: 'Maestria en I.A',
-        workGrades: [
-            {
-                value: '1.0',
-            },
-            {
-                value: '2.9'
-            }, 
-            {
-                value: '4.5'
-            }
-        ],
-        examGrades: [
-            {
-                value: '1.0',
-            },
-            {
-                value: '2.9'
-            }, 
-            {
-                value: '4.5'
-            }
-        ],
-    }
-]
+import { courses } from '../data';
 
 function App() {
   return (
@@ -103,44 +17,45 @@ function App() {
         <HashRouter>    
             <Menu />
             <Routes>
-                <Route path='/' element={<LoginPage />}/>
-                <Route path='/Alfredo' element={<Profile />}/>
-                <Route path='/Alfredo/notas' element={
-                    <>
-                        <Profile />
-                        <Searcher />
-                        <List 
-                    render={() => courses.map(
-                        (course, index) => (
-                            <Item
-                                nombre={course.nombre}
-                                profesor={course.profesor}
-                                programa={course.programa}
-        
-                                tallerGrades={() =>  
-                                    course.workGrades.map((grade, index) => ( 
-                                        <TallerGrades 
+                <Route path='/' element={<LoginPage />}>
+                    <Route path=':slug' element={<Profile />}>
+                        <Route path='/:slug/notas' element={
+                            <>
+                                <Profile />
+                                <Searcher />
+                                <List 
+                                    render={() => courses.map(
+                                        (course, index) => (
+                                        <Item
+                                            nombre={course.nombre}
+                                            profesor={course.profesor}
+                                            programa={course.programa}
+                    
+                                            tallerGrades={() =>  
+                                                course.workGrades.map((grade, index) => ( 
+                                                    <TallerGrades 
+                                                        key={index}
+                                                        grades={grade.value}
+                                                    />
+                                                ))
+                                            }
+                                            
+                                                evaluacionGrades={ () => 
+                                                course.examGrades.map((grade, index) => (
+                                                    <EvaluacionGrades 
+                                                        key={index}
+                                                        grades={grade.value}
+                                                    />
+                                                ))
+                                            }                       
                                             key={index}
-                                            grades={grade.value}
-                                        />
-                                    ))
-                                }
-                                
-                                    evaluacionGrades={ () => 
-                                    course.examGrades.map((grade, index) => (
-                                        <EvaluacionGrades 
-                                            key={index}
-                                            grades={grade.value}
-                                        />
-                                    ))
-                                }                       
-                                key={index}
-                            />
-                        )
-                    )}
-                />
-                    </>
-            }/>
+                                        />)
+                                )}/>
+                            </>
+                        }/>
+                    </Route>
+                </Route>
+                
             </Routes>
         </HashRouter> 
     </div>
---
+++ src/data.js
@@ -0,0 +1,110 @@
+const students = [
+    {
+        name: 'Alfredo Murcia',
+        courses: [
+            'Maestría biblica',
+            'Teología del nuevo testamento'
+        ],
+        credits: '17/20',
+        slug: 'alfredo-murcia'
+    },
+    {
+        name: 'Diega Forlan',
+        courses: [
+            'Licenciatura en biblia',
+            'Inteligencia Artificial',
+            'Musica Cristiana contemporanea'
+        ],
+        credits: '05/20',
+        slug: 'diega-forlan'
+
+    }
+
+]
+const courses = [
+    {
+        nombre: 'Curso 1',
+        profesor: 'Mauro',
+        programa: 'Maestria en I.A',
+        workGrades: [
+            {
+                value: '1.0',
+            },
+            {
+                value: '2.9'
+            }, 
+            {
+                value: '4.5'
+            },
+            {
+                value: '3.2'
+            }
+        ],
+        examGrades: [
+            {
+                value: '1.0',
+            },
+            {
+                value: '2.9'
+            }, 
+            {
+                value: '4.5'
+            }
+        ],
+    },
+    {
+        nombre: 'Curso 2',
+        profesor: 'Mauro',
+        programa: 'Maestria en I.A',
+        workGrades: [
+            {
+                value: '1.0',
+            },
+            {
+                value: '2.9'
+            }, 
+            {
+                value: '4.5'
+            }
+        ],
+        examGrades: [
+            {
+                value: '1.0',
+            },
+            {
+                value: '2.9'
+            }, 
+            {
+                value: '4.5'
+            }
+        ],
+    },
+    {
+        nombre: 'Curso 3',
+        profesor: 'Mauro',
+        programa: 'Maestria en I.A',
+        workGrades: [
+            {
+                value: '1.0',
+            },
+            {
+                value: '2.9'
+            }, 
+            {
+                value: '4.5'
+            }
+        ],
+        examGrades: [
+            {
+                value: '1.0',
+            },
+            {
+                value: '2.9'
+            }, 
+            {
+                value: '4.5'
+            }
+        ],
+    }
+]
+export {courses, students}
